d624223cf6663e456a9be01e1a133cf1d2cd71bf
Kryštof Píštěk <krystof1119@gmail.com>
2019-11-08 21:07:14 +0100

    Configure webpack, typescript and babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [/*'babel-loader', */'ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "commonjs2",
+  },
+  mode: 'development',
+  devtool: 'none',
+};
